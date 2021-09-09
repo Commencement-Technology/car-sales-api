3d09ec2bbd043cf0b39262b07fe3e2949e4de8f1
Eric Souza <eric.bh18souza@gmail.com>
2021-09-09 10:10:52 -0300

    Password will be hidden when find user by id is called

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,6 +7,8 @@ import {
   Param,
   Query,
   Delete,
+  UseInterceptors,
+  ClassSerializerInterceptor
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
@@ -21,6 +23,7 @@ export class UsersController {
     this.usersService.create(body.email, body.password);
   }
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Get('/:id')
   findUser(@Param('id') id: string) {
     return this.usersService.findOne(parseInt(id));
