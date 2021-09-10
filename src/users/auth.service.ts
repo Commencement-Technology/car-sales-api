1972fc98e95042fecb0bce97a8492de0217ec835
Eric Souza <eric.bh18souza@gmail.com>
2021-09-10 17:47:26 -0300

    Auth service created

---
+++ src/users/auth.service.ts
@@ -0,0 +1,17 @@
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Injectable()
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async signup(email: string, password: string) {
+    const users = await this.usersService.find(email);
+
+    if (users.length) {
+      throw new BadRequestException('This email is already in use');
+    }
+  }
+
+  signin() {}
+}
