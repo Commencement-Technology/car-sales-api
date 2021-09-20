bd9a562d291063fab606aada1f50048b6eebfe82
Eric Souza <eric.bh18souza@gmail.com>
2021-09-20 20:00:18 -0300

    Authentication testing specs

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const cookieSession = require('cookie-session');
 
 async function bootstrap() {
