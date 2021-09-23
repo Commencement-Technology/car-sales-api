26c743ed4946e9eb964741b59c7e9675ed9f284c
Eric Souza <eric.bh18souza@gmail.com>
2021-09-23 19:40:56 -0300

    New test database created and deleted as testing runs

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -28,4 +28,22 @@ describe('Authentication System', () => {
         expect(email).toEqual(email);
       });
   });
+
+  it('signs up as a new user then gets the currently logged in user', async () => {
+    const email = 'email@email.com';
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'password' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
+  });
 });
