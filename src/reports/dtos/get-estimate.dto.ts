f7b6583a7cf8170a1dd02fc5bf38fd6695652e78
Eric Souza <eric.bh18souza@gmail.com>
2021-09-30 20:57:32 -0300

    Car value estimate developed using query builders

--- src/reports/dtos/get-estimate.dto.ts
+++ src/reports/dtos/get-estimate.dto.ts
@@ -6,32 +6,32 @@ import {
   IsLongitude,
   IsLatitude,
 } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class GetEstimateDto {
-  @IsNumber()
-  @Min(0)
-  @Max(1000000)
-  price: number;
-
   @IsString()
   make: string;
 
   @IsString()
   model: string;
 
+  @Transform(({ value }) => parseInt(value))
   @IsNumber()
   @Min(1930)
   @Max(2050)
   year: number;
 
+  @Transform(({ value }) => parseInt(value))
   @IsNumber()
   @IsLongitude()
   lng: number;
 
+  @Transform(({ value }) => parseFloat(value))
   @IsNumber()
   @IsLatitude()
   lat: number;
 
+  @Transform(({ value }) => parseFloat(value))
   @IsNumber()
   @Min(0)
   @Max(1000000)
